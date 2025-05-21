2299e65333e917921ea32f30d1f1f0d6fdef2bb4
Estefany Sanabria <estefanysanabrias@gmail.com>
2025-05-20 20:52:52 -0600

    Controlador para administrador y algunos ajustes en los modelos

---
+++ Models/Admin/DashboardResumenDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace APITicketPro.Models.Admin
+{
+    public class DashboardResumenDTO
+    {
+        public int TicketsNoAsignados { get; set; }
+        public int TicketsEnProgreso { get; set; }
+        public int TicketsCriticos { get; set; }
+        public int TicketsResueltos { get; set; }
+    }
+}
--- Models/usuario_externo.cs
+++ Models/usuario_externo.cs
@@ -1,14 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace APITicketPro.Models
+public class usuario_externo
 {
-    public class usuario_externo
-    {
-        [Key]
-        public int id_usuario_externo { get; set; }
-        public int id_usuario { get; set; }
-        public string nombre { get; set; }
-        public string apellido { get; set; }
-        public string empresa { get; set; }
-    }
+    [Key]
+    public int id_usuario_externo { get; set; }
+
+    public int id_usuario { get; set; }
+
+    [ForeignKey("id_usuario")]
+    public usuario usuario { get; set; }
+
+    public string nombre { get; set; }
+    public string apellido { get; set; }
+    public string empresa { get; set; }
 }
