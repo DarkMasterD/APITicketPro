35eebdfaa8054943c7be03a64c68bc20bf76e373
Angel Magana <maganaangel342@gmail.com>
2025-05-27 13:39:31 -0600

    Agregando id de usuario interno al progreso ticket desde admin

---
+++ Models/Admin/CrearTareaDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace APITicketPro.Models.Admin
+{
+    public class CrearTareaDTO
+    {
+        public int IdTicket { get; set; }
+        public int IdTecnico { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
